cee99ba2cb6e67ca8b9ee2f712f236737f0e84d7
Abhishek Nage <anage@deakin.edu.au>
2021-01-08 22:39:47 -0700

    Changed colors of buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty("--vh", `${vh}px`);
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vw = window.innerWidth * 0.01;
+// console.log(window.innerWidth);
+// Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty("--vw", `${vw}px`);
+
 const buttons = document.querySelectorAll("button");
 
 let displayValue = "";
